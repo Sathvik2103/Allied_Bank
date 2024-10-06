07a9df0e580a2eef4a215c39bc96f19cced8c170
Sathvik Bhat <bhatsathvik21@gmail.com>
2024-10-06 23:25:07 +0530

    Add banking files

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+const SignUp = async () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      
+    </section>
+  )
+}
+
+export default SignUp
---
+++ app/(root)/page.tsx
@@ -0,0 +1,36 @@
+  import HeaderBox from '@/components/HeaderBox'
+  import TotalBalanceBox from '@/components/TotalBalanceBox';
+  import RightSidebar from '@/components/RightSidebar';
+  const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+    const loggedIn = {firstName: 'Sathvik', lastName: 'Bhat', email: 'bhatsathvik21@gmail.com'};
+
+    return (
+      <section className="home">
+        <div className="home-content">
+          <header className="home-header">
+            <HeaderBox 
+              type="greeting"
+              title="Welcome"
+              user={loggedIn?.firstName || 'Guest'}
+              subtext="Access and manage your account and transactions efficiently."
+            />
+            <TotalBalanceBox
+              accounts = {[]}
+              totalBanks = {1}
+              totalCurrentBalance = {1250.35}
+            />
+
+          </header>
+          Recent Transcations
+        </div>
+        <RightSidebar 
+          user = {loggedIn}
+          transactions = {[]}
+          banks = {[{currentBalance: 123.45},{currentBalance: 1234.45}]}
+
+        />
+      </section>
+    )
+  }
+
+  export default Home
